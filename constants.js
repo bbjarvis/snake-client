c56f9c3710d0179852f5ae9058c3a310bd55cbce
bbjarvs <brettbjarvis@gmail.com>
2021-08-22 02:35:21 +0000

    removed extra constants from constants.js, and changed exported object to be easier to use, added consts variable to input.js as well as replacing switch statement with consts[key]

--- constants.js
+++ constants.js
@@ -3,25 +3,15 @@
 
 const IP = "localhost";
 const PORT = 3001;
-const MOVE_UP_KEY = 'w';
-const MOVE_LEFT_KEY = 'a';
-const MOVE_LEFT_MSG = 'Move: up';
-const MOVE_DOWN_KEY = 's';
-const MOVE_RIGHT_KEY = 'd';
-const GOOD_JOB_KEY = 'g';
-const AWESOME_KEY = 'e';
-const TOO_BAD_KEY = 't';
-
-
 
 module.exports = {
   IP,
   PORT,
   'w': 'Move: up',
-  MOVE_LEFT_KEY,
-  MOVE_DOWN_KEY,
-  MOVE_RIGHT_KEY,
-  GOOD_JOB_KEY,
-  AWESOME_KEY,
-  TOO_BAD_KEY
+  'a': 'Move: left',
+  's': 'Move: down',
+  'd': 'Move: right',
+  'g': 'Say: Good Job',
+  'e': 'Say: AWESOME!!!',
+  't': 'Say: Too Bad',
 };
--- input.js
+++ input.js
@@ -1,6 +1,6 @@
 const { constants } = require("buffer");
-const { MOVE_UP_KEY, MOVE_LEFT_KEY, MOVE_DOWN_KEY, MOVE_RIGHT_KEY, GOOD_JOB_KEY, AWESOME_KEY, TOO_BAD_KEY } = require("./constants");
-
+const consts = require("./constants");
+// const consts = require('./constants')
 // stores the active TCP connection object
 let connection;
 /**
@@ -15,33 +15,10 @@ let connection;
   stdin.resume();
   stdin.on("data", (key) => {
     handleUserInput(key);
-
-    // input commands, getting logged into 'connection' so they can go to play.js
-    switch(key) {
-      // movement keys
-      case MOVE_UP_KEY:
-        connection.write('Move: up');
-        break;
-      case MOVE_LEFT_KEY:
-        connection.write('Move: left');
-        break;
-      case MOVE_DOWN_KEY:
-        connection.write('Move: down');
-        break;
-      case MOVE_RIGHT_KEY:
-        connection.write('Move: right');
-        break;
-      // message keys
-      case GOOD_JOB_KEY:
-        connection.write('Say: Good Job');
-        break;
-      case AWESOME_KEY:
-        connection.write('Say: AWESOME!!!');
-        break;
-      case TOO_BAD_KEY:
-        connection.write('Say: Too Bad');
-    }
     
+    // input commands, getting logged into 'connection' so they can go to play.js
+    connection.write(consts[key]);
+
     return stdin;
   })
 
