7a7bb041ab253c523e6c0b2b0570f8f21e22d09f
bbjarvs <brettbjarvis@gmail.com>
2021-08-22 01:03:30 +0000

    added message commands within the same switch statement as the movement ones  to to be displayed on the snake screen

--- input.js
+++ input.js
@@ -1,10 +1,11 @@
-
-
+// stores the active TCP connection object
+let connection;
 /**
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
- const setupInput = function() {
+ const setupInput = function(conn) {
+   connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -12,9 +13,32 @@
   stdin.on("data", (key) => {
     handleUserInput(key);
 
-    // input commands, like move
-    console.log(key==='w'?'up':key==='a'?'left':key==='s'?'down':key==='d'?'down':'');
-
+    // input commands, getting logged into 'connection' so they can go to play.js
+    switch(key) {
+      // movement keys
+      case 'w':
+        connection.write('Move: up');
+        break;
+      case 'a':
+        connection.write('Move: left');
+        break;
+      case 's':
+        connection.write('Move: down');
+        break;
+      case 'd':
+        connection.write('Move: right');
+        break;
+      // message keys
+      case 'g':
+        connection.write('Say: Good Job');
+        break;
+      case 'e':
+        connection.write('Say: AWESOME!!!');
+        break;
+      case 't':
+        connection.write('Say: Too Bad');
+    }
+    
     return stdin;
   })
 
